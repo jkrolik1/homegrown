8ef9d1032eaa39d5674c578529a722509463f365
jkrolik1 <56318896+jkrolik1@users.noreply.github.com>
2020-05-16 06:39:29 +0200

    sound

--- tutorial1.cpp
+++ tutorial1.cpp
@@ -18,6 +18,8 @@
 #include <stack>
 #include <set>
 #include <unordered_map>
+#include <windows.h>            // sound
+#include <mmsystem.h>           // sound
 
 #include <boost/lexical_cast.hpp>
 #include <boost/variant.hpp>
@@ -157,10 +159,65 @@ void bimap();                           // bimap
 void heap();                            // heap / priority queue / binomial heap
 void bArray();                          // array
 void bSwap();                           // swap
+void timer1min();               // learning method
+void soundLearn();              // learning method
+void soundBreak();              // learning method
+void learningMethod();          // learning method
 
 int main(){
 
     return 0;
+}
+
+void learningMethod(){
+    while(1){
+
+        soundBreak();
+
+        for(int i=0; i<3; ++i)
+            timer1min();
+
+        soundLearn();
+
+        for(int i=0; i<15; ++i)
+            timer1min();
+    }
+}
+
+void soundLearn(){
+    PlaySound(TEXT("start.wav"), NULL, SND_SYNC);
+}
+
+void soundBreak(){
+    PlaySound(TEXT("stop.wav"), NULL, SND_SYNC);
+}
+
+void timer1min(){
+    unsigned int x_hours=0;
+	unsigned int x_minutes=0;
+	unsigned int x_seconds=0;
+	unsigned int x_milliseconds=0;
+	unsigned int totaltime=0;
+	unsigned int count_down_time_in_secs=0;
+	unsigned int time_left=0;
+
+    clock_t x_startTime,x_countTime;
+	count_down_time_in_secs=60;  // 1 minute is 60, 1 hour is 3600
+
+    x_startTime=clock();  // start clock
+    time_left=count_down_time_in_secs-x_seconds;   // update timer
+
+	while (time_left>0){
+		x_countTime=clock(); // update timer difference
+		x_milliseconds=x_countTime-x_startTime;
+		x_seconds=(x_milliseconds/(CLOCKS_PER_SEC))-(x_minutes*60);
+		x_minutes=(x_milliseconds/(CLOCKS_PER_SEC))/60;
+		x_hours=x_minutes/60;
+
+		time_left=count_down_time_in_secs-x_seconds; // subtract to get difference
+
+		std::cout << time_left << " ";
+	}
 }
 
 void bSwap(){
